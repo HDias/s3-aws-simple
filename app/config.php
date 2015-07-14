3602670b248b61d5815da164f690bb7026810a47
Horecio Araújo Dias <horecio@gmail.com>
2015-07-14 15:27:07 -0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -2,8 +2,8 @@
 
 return [
     's3' => [
-        'key' => 'AKIAJR2JO5T4D4SAIK5A',
-        'secret' => '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',
-        'bucket' => 'horecio'
+        'key' => '',
+        'secret' => '',
+        'bucket' => ''
     ]
 ];
